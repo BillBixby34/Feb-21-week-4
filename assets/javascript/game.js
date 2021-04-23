43ac7e564a0f8fe007f06e2f11a5a7e7183196e2
Will H <w.j.hilliard28@gmail.com>
2021-04-23 14:30:07 -0400

    dynamic element creation with jquery, other jquery manipulation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,74 @@
 $(document).ready(function(){
 
     //variables
-    let rock;
+    let rocks = ["coral", "sedimentary", "metamorphic", "igneous"];
     let playerTotal;
-    let opsNumber;
+    let opsNumber = "";
     let wins;
     let losses;
+    let rockValue = [];//will hold value to be placed in each rock button
+    isRockOn = false;
 
 //if player goes over opsNumber, game ends
 //on click of rock, add that number to playerTotal
 
-//function to randomize opsNumber between 19-120
-function randomNumber() {
-   const random = Math.floor(Math.random() * (121 - 19) + 19);
+//function to randomize values
+function randomNumber(min, max) {
+   return Math.floor(Math.random() * (max - min) + min);
 
-   opsNumber = random + opsNumber;
+   //opsNumber = randomNumber(19, 121);
+  // opsNumber = random + opsNumber;
    //prepend variable opsNumber to #op-score element with each click of #lottoButton
-   $("#op-score").html("<h1>" + opsNumber + "</h1>");
+   //$("#op-score").html("<h1>" + opsNumber + "</h1>");
   };
 
+    
+
+
+    function randomOps(){
+       // randomNumber(19, 121);
+        let random = randomNumber(19, 121);
+        opsNumber = random + opsNumber;
+
+      $("#op-score").html("<h1>" + opsNumber + "</h1>");
+      //console.log(randomOps())
+    };
+
+    //MAY ATTEMPT TO LOOP IF FOREACH DOESNT WORKOUT
+
+     
+ //}
+
+  //randomly generate number for each rock between 1-12
+  function randomRocks(){
+      //rocks.forEach(() => {
+        //  rockValue = randomNumber(1, 13);
+       //  console.log(rockValue);
+          //
+        //});
+     for (let i = 0; i < rocks.length; i++) {
+     const rockBtn = $("<button>");
+     rockBtn.addClass("col-md-3 btn" );
+     rockBtn.attr("rock-type", rocks[i]);
+     rockBtn.text(rocks[i]);
+     //append each "rockBtn" to the ".rock-row" div (provided).
+     $(".rock-row").append(rockBtn);
+    }
+  }
+  //set array with length of 3, to iterate over function until <4
+
+  //make operator variable binary
+  //should turn on once rock button is pressed
+  //from there will add the Rock Value assigned to each rock
+
+//rock button functionality
+$( ".rock" ).on("click", function() {
+    isRockOn = true;
+    alert( "Rock time!" );
+    addCrystal();//function to addto players total
+    alert("You're total is " + playerTotal + "!")
+
+});
 
 //function to reset game
 function restartRocks(){
@@ -27,8 +77,8 @@ function restartRocks(){
     losses = 0;
     playerTotal = 0;
     opsNumber = 0;
-    randomNumber();
-
+    randomOps();
+    randomRocks();
 };
 
 $(".reset").on("click", function () {
