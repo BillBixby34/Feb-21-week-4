ac1b8164b1a803f7d2726ee94dceb4c503da6c58
Will H <w.j.hilliard28@gmail.com>
2021-04-27 14:35:22 -0400

    using jquery, a dynamic element manipulation needs correction.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,31 +2,40 @@ $(document).ready(function(){
 
     //variables
     let rocks = ["coral", "sedimentary", "metamorphic", "igneous"];
+    let rock;
     let playerTotal;
     let opsNumber = "";
     let wins;
     let losses;
-    let rockValue = [];//will hold value to be placed in each rock button
-    isRockOn = false;
+    let rockValue;//will hold value to be placed in each rock button
+    let isRockOn;//will hold win-loss values, NOT reset
+    let rocksMore;//if player score is greater than opponent
 
 //if player goes over opsNumber, game ends
 //on click of rock, add that number to playerTotal
 
+//function to initialize/reset game
+    function restartRocks(){
+        //clear .rock-row div
+        $("#rocks").empty();
+        alert("Game start!")
+        wins = "";
+        losses = "";
+        playerTotal = "";
+        opsNumber = "";
+        rockValue = [];
+        isRockOn = false;
+        rocksMore = false;
+        randomOps();
+        randomRocks();
+    }; 
 //function to randomize values
 function randomNumber(min, max) {
    return Math.floor(Math.random() * (max - min) + min);
 
-   //opsNumber = randomNumber(19, 121);
-  // opsNumber = random + opsNumber;
-   //prepend variable opsNumber to #op-score element with each click of #lottoButton
-   //$("#op-score").html("<h1>" + opsNumber + "</h1>");
   };
 
-    
-
-
     function randomOps(){
-       // randomNumber(19, 121);
         let random = randomNumber(19, 121);
         opsNumber = random + opsNumber;
 
@@ -34,52 +43,40 @@ function randomNumber(min, max) {
       //console.log(randomOps())
     };
 
-    //MAY ATTEMPT TO LOOP IF FOREACH DOESNT WORKOUT
-
-     
- //}
-
   //randomly generate number for each rock between 1-12
   function randomRocks(){
-      //rocks.forEach(() => {
-        //  rockValue = randomNumber(1, 13);
-       //  console.log(rockValue);
-          //
-        //});
+      
      for (let i = 0; i < rocks.length; i++) {
-     const rockBtn = $("<button>");
-     rockBtn.addClass("col-md-3 btn" );
-     rockBtn.attr("rock-type", rocks[i]);
-     rockBtn.text(rocks[i]);
-     //append each "rockBtn" to the ".rock-row" div (provided).
-     $(".rock-row").append(rockBtn);
-    }
+        
+        const rockBtn = $("<img>");
+        rockBtn.attr('src', "assets/images/" + rocks[i] + ".png");//displays rock image
+        rockBtn.addClass(" col-sm-3 btn rock-btn" )
+        //.attr("rock-type", rocks[i]);
+        //give each rockBtn an attr 'value' equal to rockValue
+        rockValue[i] = randomNumber(1, 13);
+        rockBtn.attr('value', rockValue[i] )
+        console.log("The value of the " + rocks[i] + " is " + rockValue[i] + "!")
+        //append each "rockBtn" to the ".rock-row" div (provided).
+        $("#rocks").append(rockBtn);//when indexed, only shows 1st
+     }
   }
-  //set array with length of 3, to iterate over function until <4
-
-  //make operator variable binary
+   //.rock is NOT registering clicks on image.  
   //should turn on once rock button is pressed
   //from there will add the Rock Value assigned to each rock
 
 //rock button functionality
-$( ".rock" ).on("click", function() {
-    isRockOn = true;
-    alert( "Rock time!" );
-    addCrystal();//function to addto players total
-    alert("You're total is " + playerTotal + "!")
+$(".rock-btn").on("click", function(){
+    //add rock value to player score
+    //playerTotal = playerTotal + this.value;//define this.value??
+    //console.log("player has " + playerTotal + " points")
+    //isRockOn = true;
+    console.log( "Rock time!" );
+    //addCrystal();//function to addto players total
+    //alert("You're total is " + playerTotal + "!")
 
-});
+})
 
-//function to reset game
-function restartRocks(){
-    alert("Game start!")
-    wins = 0;
-    losses = 0;
-    playerTotal = 0;
-    opsNumber = 0;
-    randomOps();
-    randomRocks();
-};
+
 
 $(".reset").on("click", function () {
         restartRocks();
